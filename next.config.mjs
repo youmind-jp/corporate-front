ec9795693f0c1e0b528a5fc8d2d0f9c86c051481
AnikaBristy <bristy.technext@gmail.com>
2025-05-22 13:03:19 +0600

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
     output: 'export',
+    basePath: isProd ? "/crypgo" : "",
+    assetPrefix: isProd ? "/crypgo/" : "",
+    images: {
+        unoptimized: true,
+    },
+
 };
 
 export default nextConfig;
+
