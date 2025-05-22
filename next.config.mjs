625435f3d28e6e9cc8d84aa62f550e6d2d4b4cef
AnikaBristy <bristy.technext@gmail.com>
2025-05-22 14:19:06 +0600

    updated build directory

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
     output: 'export',
+    distDir: 'build',
     basePath: isProd ? "/crypgo" : "",
     assetPrefix: isProd ? "/crypgo/" : "",
     images: {
