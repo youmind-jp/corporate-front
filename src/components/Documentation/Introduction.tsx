cd3c913159c51d3c689c367fb6e35c4fd9271f4b
AnikaBristy <bristy.technext@gmail.com>
2025-05-25 15:10:19 +0600

    updated

--- src/components/Documentation/Introduction.tsx
+++ src/components/Documentation/Introduction.tsx
@@ -74,7 +74,7 @@ export const Introduction = () => {
                   className="lg:w-1/5 md:w-full text-center lg:border-b-0 border-b lg:border-e lg:last:border-e-0 last:border-b-0 border-dark_border border-opacity-60"
                 >
                   <Image
-                    src= {`${getImagePrefix()}${item.img}`}
+                    src= {item.img}
                     alt="npm-package"
                     className=" mx-auto w-10 h-10 "
                   />
--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -1,10 +1,13 @@
 import { HeaderItem } from "@/types/menu";
+import { getImagePrefix } from "@/utils/utils";
+
+
 
 export const headerData: HeaderItem[] = [
-  { label: "Buy & Sell", href: "/#main-banner" },
-  { label: "Development", href: "/#development" },
-  { label: "Work", href: "/#work" },
-  { label: "Portfolio", href: "/#portfolio" },
-  { label: "Upgrade", href: "/#upgrade" },
-  { label: "Docs", href: "/documentation#version" },
+  { label: "Buy & Sell", href: `${getImagePrefix()}/#main-banner`},
+  { label: "Development", href: `${getImagePrefix()}/#development` },
+  { label: "Work", href: `${getImagePrefix()}/#work` },
+  { label: "Portfolio", href: `${getImagePrefix()}/#portfolio` },
+  { label: "Upgrade", href: `${getImagePrefix()}/#upgrade` },
+  { label: "Docs", href: `${getImagePrefix()}/documentation#version` },
 ];
