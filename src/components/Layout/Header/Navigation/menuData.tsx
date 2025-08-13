2e21ace920886aa04df02ccafd803af5aaeaa37b
武田 憲太郎 <takeda@youmind.jp>
2025-08-13 12:45:17 +0900

    ナビメニュー見出し

--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -4,10 +4,10 @@ import { getImagePrefix } from "@/utils/utils";
 
 
 export const headerData: HeaderItem[] = [
-  { label: "Buy & Sell", href: "/#main-banner" },
-  { label: "Development", href: "/#development" },
-  { label: "Work", href: "/#work" },
-  { label: "Portfolio", href: "/#portfolio" },
-  { label: "Upgrade", href: "/#upgrade" },
-  { label: "Docs", href: "/documentation#version" },
+  { label: "Services", href: "/#main-banner" },
+  { label: "Case Studies", href: "/#development" },
+  { label: "Expertise", href: "/#work" },
+  { label: "About", href: "/#portfolio" },
+  { label: "Director", href: "/#upgrade" },
+  { label: "Contact", href: "/documentation#version" },
 ];
