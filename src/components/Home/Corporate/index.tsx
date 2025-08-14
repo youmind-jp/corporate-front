4bc201459365f9dbf7dbc3ae497d8148efbe76d6
武田 憲太郎 <takeda@youmind.jp>
2025-08-14 13:22:41 +0900

    会社概要のデザイン改善とコンテンツ追加

--- src/components/Home/Corporate/index.tsx
+++ src/components/Home/Corporate/index.tsx
@@ -21,49 +21,45 @@ const Corporate = () => {
         transition={{ duration: 0.6 }}
       >
         <div className="container mx-auto lg:max-w-screen-xl px-4">
-          <div className="mb-8">
-            <dl className="w-full bg-dark_grey bg-opacity-60 max-w-3xl mx-auto rounded-xl overflow-hidden">
-              <div className="grid grid-cols-2 border-b border-slateGray">
-                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80">会社名</dt>
-                <dd className="px-6 py-4 text-white">株式会社ユーマインド</dd>
-              </div>
-              <div className="grid grid-cols-2 border-b border-slateGray">
-                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80">英文表記</dt>
-                <dd className="px-6 py-4 text-white">YouMind</dd>
-              </div>
-              <div className="grid grid-cols-2 border-b border-slateGray">
-                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80">代表者</dt>
-                <dd className="px-6 py-4 text-white">武田 憲太郎</dd>
-              </div>
-              <div className="grid grid-cols-2 border-b border-slateGray">
-                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80">所在地</dt>
-                <dd className="px-6 py-4 text-white">〒158-0083 東京都 世田谷区 奥沢 8-26-24</dd>
-              </div>
-              <div className="grid grid-cols-2 border-b border-slateGray">
-                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80">TEL</dt>
-                <dd className="px-6 py-4 text-white">03-6805-9480</dd>
-              </div>
-              <div className="grid grid-cols-2 border-b border-slateGray">
-                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80">設立</dt>
-                <dd className="px-6 py-4 text-white">2004年8月30日</dd>
-              </div>
-              <div className="grid grid-cols-2 border-b border-slateGray">
-                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80">資本金</dt>
-                <dd className="px-6 py-4 text-white">1,000万円</dd>
-              </div>
-              <div className="grid grid-cols-2 border-b border-slateGray">
-                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80">従業員数</dt>
-                <dd className="px-6 py-4 text-white">6名（役員除く）</dd>
-              </div>
-              <div className="grid grid-cols-2">
-                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80">企業理念</dt>
-                <dd className="px-6 py-4 text-white">
-                  あなたの意思を尊重します。<br />
-                  <span className="text-muted text-16 italic">“We respect YouMind.”</span>
-                </dd>
-              </div>
-            </dl>
-          </div>
+            <div className="mb-8">
+              <dl className="w-full grid [grid-template-columns:30%_70%] bg-dark_grey bg-opacity-60 max-w-3xl mx-auto rounded-xl overflow-hidden">
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 border-b border-slateGray min-h-12 sm:min-h-10 min-w-[10rem]">会社名</dt>
+                <dd className="px-6 py-4 text-white border-b border-slateGray">株式会社ユーマインド</dd>
+
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 border-b border-slateGray min-h-12 sm:min-h-10 min-w-[10rem]">英文表記</dt>
+                <dd className="px-6 py-4 text-white border-b border-slateGray">YouMind</dd>
+
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 border-b border-slateGray min-h-12 sm:min-h-10 min-w-[10rem]">代表者</dt>
+                <dd className="px-6 py-4 text-white border-b border-slateGray">武田 憲太郎</dd>
+
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 border-b border-slateGray min-h-12 sm:min-h-10 min-w-[10rem]">所在地</dt>
+                <dd className="px-6 py-4 text-white border-b border-slateGray">〒158-0083 東京都 世田谷区 奥沢 8-26-24</dd>
+
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 border-b border-slateGray min-h-12 sm:min-h-10 min-w-[10rem]">TEL</dt>
+                <dd className="px-6 py-4 text-white border-b border-slateGray">03-6805-9480</dd>
+
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 border-b border-slateGray min-h-12 sm:min-h-10 min-w-[10rem]">設立</dt>
+                <dd className="px-6 py-4 text-white border-b border-slateGray">2004年8月30日</dd>
+
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 border-b border-slateGray min-h-12 sm:min-h-10 min-w-[10rem]">資本金</dt>
+                <dd className="px-6 py-4 text-white border-b border-slateGray">1,000万円</dd>
+
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 border-b border-slateGray min-h-12 sm:min-h-10 min-w-[10rem]">法人番号</dt>
+                <dd className="px-6 py-4 text-white border-b border-slateGray">9011001056484</dd>
+
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 border-b border-slateGray min-h-12 sm:min-h-10 min-w-[10rem]">登録番号</dt>
+                <dd className="px-6 py-4 text-white border-b border-slateGray">T9011001056484</dd>
+
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 border-b border-slateGray min-h-12 sm:min-h-10 min-w-[10rem]">D-U-N-S</dt>
+                <dd className="px-6 py-4 text-white border-b border-slateGray">710475430</dd>
+
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 border-b border-slateGray min-h-12 sm:min-h-10 min-w-[10rem]">従業員数</dt>
+                <dd className="px-6 py-4 text-white border-b border-slateGray">6名（役員除く）</dd>
+
+                <dt className="px-6 py-4 font-semibold text-white bg-dark_grey/80 min-h-12 sm:min-h-10 min-w-[10rem]">企業理念</dt>
+                <dd className="px-6 py-4 text-white">あなたの意思を尊重します。<br /><span className="text-muted text-16 italic">“We respect YouMind.”</span></dd>
+              </dl>
+            </div>
         </div>
       </motion.div>
     </section>
